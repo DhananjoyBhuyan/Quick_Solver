3f43dd72bbbe3108e4cb8a00529bc0799deaae49
Dhananjoy Bhuyan <dhananjoy.bhuyan.2022@gmail.com>
2025-04-05 06:27:26 +0530

    Update quick_solver_installer.sh

--- quick_solver_installer.sh
+++ quick_solver_installer.sh
@@ -67,29 +67,4 @@ echo " "
 echo " "
 echo "You can find the game in your all-applications menu!!!"
 echo " "
-echo "Do you want the game in your desktop as well?? Or just stick to all-applications menu??"
-echo "Enter Y for yes and N for no"
-read -p "Y/N: " desktopisgood
-if [[ "$desktopisgood" =~ ^[Nn]$ ]]; then
-    echo "Alright, so installation is done!! You can find the game in your all-applications menu."
-    exit 1
-fi
 
-echo " "
-cd ~/Desktop
-rm -rf ~/Desktop/quick_solver.desktop
-wget https://raw.githubusercontent.com/DhananjoyBhuyan/Quick_Solver/main/quick_solver.desktop
-chmod +x ./quick_solver.desktop
-chmod +x ./quick_solver.desktop
-gio set ~/Desktop/quick_solver.desktop metadata::trusted true
-chmod +x ./quick_solver.desktop
-chmod +x ./quick_solver.desktop
-sed -i "s|\$HOME|$HOME|g" ~/Desktop/quick_solver.desktop
-chmod +x ./quick_solver.desktop
-chmod +x ./quick_solver.desktop
-chmod +x ./quick_solver.desktop
-echo " "
-echo "Great!! You can also find the game icon in your desktop!!!!!"
-echo " "
-echo " "
-echo " "

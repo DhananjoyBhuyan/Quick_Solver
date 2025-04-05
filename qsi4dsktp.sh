49cf26e462376413773430973e4e2c98bc9d8a5e
Dhananjoy Bhuyan <dhananjoy.bhuyan.2022@gmail.com>
2025-04-05 06:30:57 +0530

    Create qsi4dsktp.sh

---
+++ qsi4dsktp.sh
@@ -0,0 +1,17 @@
+cd ~/Desktop
+rm -rf ~/Desktop/quick_solver.desktop
+wget https://raw.githubusercontent.com/DhananjoyBhuyan/Quick_Solver/main/quick_solver.desktop
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+gio set ~/Desktop/quick_solver.desktop metadata::trusted true
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+sed -i "s|\\$HOME|$HOME|g" ~/Desktop/quick_solver.desktop
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+echo " "
+echo "Great!! You can also find the game icon in your desktop!!!!!"
+echo " "
+echo " "
+echo " "

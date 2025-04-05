6a78ce96c60f9814da43e0af7d4ad62a76570155
Dhananjoy Bhuyan <dhananjoy.bhuyan.2022@gmail.com>
2025-04-05 06:23:19 +0530

    Update qsi4update.sh

--- qsi4update.sh
+++ qsi4update.sh
@@ -31,4 +31,20 @@ echo " "
 echo " "
 echo "You can find the game in your all-applications menu!!!"
 echo " "
+cd ~/Desktop
 rm -rf ~/Desktop/quick_solver.desktop
+wget https://raw.githubusercontent.com/DhananjoyBhuyan/Quick_Solver/main/quick_solver.desktop
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+gio set ~/Desktop/quick_solver.desktop metadata::trusted true
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+sed -i "s|\\$HOME|$HOME|g" ~/Desktop/quick_solver.desktop
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+chmod +x ./quick_solver.desktop
+echo " "
+echo "Great!! You can also find the game icon in your desktop!!!!!"
+echo " "
+echo " "
+echo " "
